70d3abb695fd1417c58e046c4f20e6cb7e0a0c2d
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-07-21 20:20:49 +0900

    [Bronze III] Title: Deja vu of Go Players, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/23663. Deja vu of Go Players/Deja vu of Go Players.py
@@ -0,0 +1,9 @@
+for t in range(int(input())):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    if n > m:
+        print("No")
+    else:
+        print("Yes")
