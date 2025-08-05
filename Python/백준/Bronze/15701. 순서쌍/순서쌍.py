8a30e70a93814219eaf50cad57a2ffd0ae7e245f
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-08-05 19:35:06 +0900

    [Bronze II] Title: 순서쌍, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/15701. 순서쌍/순서쌍.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+ans = 0
+for i in range(1, int(n ** 0.5) + 1):
+    if n % i == 0:
+        if i ** 2 == n:
+            ans += 1
+        else:
+            ans += 2
+
+print(ans)
