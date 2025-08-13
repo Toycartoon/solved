906e7ca699fcabdfe9f04c07b829a37a34959409
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-08-13 22:34:54 +0900

    [Silver III] Title: 문자열 암호화, Time: 148 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Silver/6616. 문자열 암호화/문자열 암호화.py
+++ Python/백준/Silver/6616. 문자열 암호화/문자열 암호화.py
@@ -6,14 +6,12 @@ while True:
     s = "".join(input().split()).upper()
     ans = ["" for _ in range(len(s))]
 
-    idx = 0
     v = 0
-    while idx < len(s):
+    for idx in range(len(s)):
         if v >= len(ans):
             v = ans.index("")
 
         ans[v] = s[idx]
-        idx += 1
         v += n
 
     print(*ans, sep="")
