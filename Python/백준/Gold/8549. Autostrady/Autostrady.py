f936394b1aafe3507ad51cda14c08882803e7b89
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-08-19 17:01:43 +0900

    [Gold IV] Title: Autostrady, Time: 396 ms, Memory: 52716 KB -BaekjoonHub

---
+++ Python/백준/Gold/8549. Autostrady/Autostrady.py
@@ -0,0 +1,47 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+def find(i):
+    trace = []
+    ptr = i
+    while arr[ptr] != -1:
+        trace.append(ptr)
+        ptr = arr[ptr]
+
+    for v in trace:
+        arr[v] = ptr
+
+    return ptr
+
+
+def union(i, j):
+    ir = find(i)
+    jr = find(j)
+
+    if ir == jr:
+        return False
+
+    arr[jr] = ir
+    return True
+
+
+q = []
+n, m = map(int, input().split())
+
+arr = [-1] * (n + 1)
+w = []
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    heapq.heappush(q, (c, a, b))
+
+while q:
+    k, i, j = heapq.heappop(q)
+
+    f = union(i, j)
+    if f:
+        w.append(k)
+
+print(max(w))
