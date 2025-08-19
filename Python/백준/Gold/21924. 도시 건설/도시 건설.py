3cd4dd0db1cb5be623e99c16538c7ab8b6e70bd0
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-08-19 15:26:48 +0900

    [Gold IV] Title: 도시 건설, Time: 2684 ms, Memory: 119212 KB -BaekjoonHub

---
+++ Python/백준/Gold/21924. 도시 건설/도시 건설.py
@@ -0,0 +1,49 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+def find(i):
+    trace = []
+    ptr = i
+    while arr[ptr] != -1:
+        trace.append(ptr)
+        ptr = arr[ptr]
+
+    for v in trace:
+        arr[v] = ptr
+
+    return ptr
+
+
+def union(i, j):
+    ir = find(i)
+    jr = find(j)
+
+    if ir == jr:
+        return False
+
+    arr[jr] = ir
+    return True
+
+
+q = []
+n, m = map(int, input().split())
+
+arr = [-1] * (n + 1)
+w = 0
+v = 0
+
+for _ in range(m):
+    i, j, k = map(int, input().split())
+    v += k
+    heapq.heappush(q, (k, i, j))
+
+while q:
+    k, i, j = heapq.heappop(q)
+
+    f = union(i, j)
+    if f:
+        w += k
+
+print(v-w if arr.count(-1) == 2 else -1)
