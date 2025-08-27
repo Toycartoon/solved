881e4852c9daa8fcf1378102a09701256c51714b
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-08-27 23:35:17 +0900

    [Silver V] Title: 알고리즘 수업 - 퀵 정렬 1, Time: 408 ms, Memory: 321208 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/24090. 알고리즘 수업 － 퀵 정렬 1/알고리즘 수업 － 퀵 정렬 1.py
@@ -0,0 +1,44 @@
+import sys
+
+sys.setrecursionlimit(20 ** 4)
+
+def partition(p, r):
+    global ex, ans
+
+    x = a[r]
+    i = p - 1
+    for j in range(p, r):
+        if a[j] <= x:
+            i += 1
+            a[i], a[j] = a[j], a[i]
+            ex += 1
+            if ex == k:
+                ans = (a[i], a[j])
+
+    if i + 1 != r:
+        a[i+1], a[r] = a[r], a[i+1]
+        ex += 1
+        if ex == k:
+            ans = (a[i+1], a[r])
+
+    return i + 1
+
+
+def quick_sort(p, r):
+    if p < r:
+        q = partition(p, r)
+        quick_sort(p, q-1)
+        quick_sort(q+1, r)
+
+
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+ex = 0
+ans = ()
+quick_sort(0, n-1)
+
+if len(ans) == 0:
+    print(-1)
+else:
+    print(*ans)
