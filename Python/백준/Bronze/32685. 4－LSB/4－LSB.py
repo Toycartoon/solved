7e798bf1992d1efb164d7b790a0dfd34c956b506
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-08-31 17:11:30 +0900

    [Bronze II] Title: 4-LSB, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- Python/백준/Bronze/32685. 4－LSB/4－LSB.py
+++ Python/백준/Bronze/32685. 4－LSB/4－LSB.py
@@ -1,4 +1,1 @@
-a = bin(int(input()))
-b = bin(int(input()))
-c = bin(int(input()))
-print(str(int(a[2:][-4:].zfill(4)+b[2:][-4:].zfill(4)+c[2:][-4:].zfill(4),2)).zfill(4))
+print(str(int(bin(int(input()))[2:][-4:].zfill(4)+bin(int(input()))[2:][-4:].zfill(4)+bin(int(input()))[2:][-4:].zfill(4),2)).zfill(4))
