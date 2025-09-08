f0cd303f42f06429138e9df268e7c85b23fccfdd
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-09-09 01:53:36 +0900

    [Silver V] Title: Spring 2005: Arriving at USC, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/11015. Spring 2005： Arriving at USC/Spring 2005： Arriving at USC.py
@@ -0,0 +1,24 @@
+import sys
+
+input = sys.stdin.readline
+
+for t in range(int(input())):
+    a = []
+    for n in range(int(input())):
+        a.append(input())
+    s = input().strip().upper()
+
+    print(f"Data Set {t+1}:")
+    for i in a:
+        f = True
+        x = i.upper()
+        for v in s:
+            if v in x:
+                x = x[x.index(v)+1:]
+            else:
+                f = False
+                break
+
+        if f:
+            print(i, end="")
+    print()
