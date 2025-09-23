db3c06ad50cd6d560b405c9c999988e065d7dda9
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-09-23 14:43:49 +0900

    [Bronze II] Title: Look and Say, Time: 440 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/4841. Look and Say/Look and Say.py
@@ -0,0 +1,14 @@
+for t in range(int(input())):
+    s = input()
+    a = []
+    v = 1
+    for i in range(1, len(s)):
+        if s[i-1] == s[i]:
+            v += 1
+        else:
+            a.append(v)
+            a.append(s[i-1])
+            v = 1
+    a.append(v)
+    a.append(s[-1])
+    print(*a, sep="")
