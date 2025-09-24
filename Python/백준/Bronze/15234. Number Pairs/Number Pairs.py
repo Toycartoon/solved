0c17c34176616c9139fdda9650bf7b10e1f9b145
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-09-24 22:01:57 +0900

    [Bronze II] Title: Number Pairs, Time: 104 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/15234. Number Pairs/Number Pairs.py
@@ -0,0 +1,9 @@
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+ans = 0
+for i in range(n-1):
+    for j in range(i+1, n):
+        if a[i] + a[j] == k:
+            ans += 1
+print(ans)
