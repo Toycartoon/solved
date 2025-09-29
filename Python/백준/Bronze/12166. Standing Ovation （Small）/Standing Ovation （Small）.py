6d7a44305f3a84d552863cbe1f0d84f21c3b4ac8
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-09-29 14:03:31 +0900

    [Bronze II] Title: Standing Ovation (Small), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/12166. Standing Ovation （Small）/Standing Ovation （Small）.py
@@ -0,0 +1,11 @@
+for t in range(int(input())):
+    n, s = input().split()
+    ans = 0
+    c = int(s[0])
+    for i in range(1, int(n)+1):
+        if c >= i:
+            c += int(s[i])
+        else:
+            ans += i-c
+            c += int(s[i]) + 1
+    print(f"Case #{t+1}: {ans}")
