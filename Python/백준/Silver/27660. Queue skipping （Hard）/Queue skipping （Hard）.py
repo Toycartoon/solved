6fd159624bc93bc55536f36c181aafd4824702f8
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-10-03 01:41:01 +0900

    [Silver V] Title: Queue skipping (Hard), Time: 3168 ms, Memory: 41672 KB -BaekjoonHub

---
+++ Python/백준/Silver/27660. Queue skipping （Hard）/Queue skipping （Hard）.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+for t in range(int(input())):
+    _ = input()
+    n, e = map(int, input().split())
+    a = [i for i in range(n)]
+
+    idx = 0
+    for x in range(e):
+        m = int(input())
+        a[m-1] = idx
+        idx -= 1
+    print(a.index(max(a))+1)
