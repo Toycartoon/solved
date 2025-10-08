e4c53f5c6d053dcb03b79b23443068be2c8d21c6
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-10-09 01:51:10 +0900

    [Bronze II] Title: Rectangles, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/8295. Rectangles/Rectangles.py
@@ -0,0 +1,7 @@
+n, m, p = map(int, input().split())
+ans = 0
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        if (i + j) * 2 >= p:
+            ans += (n - i + 1) * (m - j + 1)
+print(ans)
