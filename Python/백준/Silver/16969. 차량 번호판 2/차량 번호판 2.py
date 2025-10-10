ff2eda7ab3a3f2c6c6be7e7eaf6e2e34ac3ed73e
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-10-10 10:48:34 +0900

    [Silver V] Title: 차량 번호판 2, Time: 380 ms, Memory: 34368 KB -BaekjoonHub

---
+++ Python/백준/Silver/16969. 차량 번호판 2/차량 번호판 2.py
@@ -0,0 +1,20 @@
+s = input()
+ans = 1
+if s[0] == "d":
+    ans *= 10
+else:
+    ans *= 26
+
+for i in range(1, len(s)):
+    if s[i] == "d":
+        if s[i-1] == "d":
+            ans *= 9
+        else:
+            ans *= 10
+    elif s[i] == "c":
+        if s[i-1] == "c":
+            ans *= 25
+        else:
+            ans *= 26
+    ans %= 1000000009
+print(ans)
