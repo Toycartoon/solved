c5cbaedf38dfaac3f49afee851d049a05eec41ca
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-10-10 14:48:14 +0900

    [Bronze I] Title: Sunflowers, Time: 1304 ms, Memory: 33432 KB -BaekjoonHub

---
+++ Python/백준/Bronze/16020. Sunflowers/Sunflowers.py
@@ -0,0 +1,27 @@
+n = int(input())
+g = [list(map(int, input().split())) for _ in range(n)]
+
+for i in range(4):
+    f = True
+    for y in range(n):
+        if not f:
+            break
+        for x in range(1, n):
+            if g[y][x-1] > g[y][x]:
+                f = False
+                break
+
+    for y in range(n):
+        if not f:
+            break
+        for x in range(1, n):
+            if list(zip(*g))[y][x-1] > list(zip(*g))[y][x]:
+                f = False
+                break
+
+    if not f:
+        g = list(zip(*g[::-1]))
+    else:
+        for v in g:
+            print(*v)
+        break
