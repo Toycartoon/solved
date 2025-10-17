5ea1caca7c2123dbb8cce1dbfa2d2aaef108702b
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-10-18 03:00:25 +0900

    [Silver IV] Title: Sudoku Verify, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Silver/34444. Sudoku Verify/Sudoku Verify.py
@@ -0,0 +1,26 @@
+a = [list(map(int, input().split())) for _ in range(9)]
+
+f = True
+for i in a:
+    if len(set(i)) != 9:
+        f = False
+        break
+
+for i in zip(*a):
+    if len(set(i)) != 9:
+        f = False
+        break
+
+for k in range(3):
+    for i in range(3, 12, 3):
+        s = set()
+        for j in range(3):
+            s.update(a[3*k+j][i-3:i])
+        if len(s) != 9:
+            f = False
+            break
+
+if f:
+    print("VALID")
+else:
+    print("INVALID!")
