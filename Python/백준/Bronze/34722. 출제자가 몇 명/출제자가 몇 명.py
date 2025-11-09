2284e9dbf5f72f72686f3c0a933ed219a9fa6fa4
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-11-09 23:58:01 +0900

    [Bronze IV] Title: 출제자가 몇 명, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/34722. 출제자가 몇 명/출제자가 몇 명.py
@@ -0,0 +1,6 @@
+ans = 0
+for i in range(int(input())):
+    s, c, a, r = map(int, input().split())
+    if s >= 1000 or c >= 1600 or a >= 1500 or (r != -1 and r <= 30):
+        ans += 1
+print(ans)
