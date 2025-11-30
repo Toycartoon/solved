1206cc59d875f9bbd9fe4c8b955479fceb93dfdb
Toycartoon <53961972+Toycartoon@users.noreply.github.com>
2025-12-01 00:21:20 +0900

    [Bronze V] Title: 공통교육과정, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/34813. 공통교육과정/공통교육과정.py
@@ -0,0 +1,9 @@
+s = input()
+if s.startswith("F"):
+    print("Foundation")
+elif s.startswith("C"):
+    print("Claves")
+elif s.startswith("V"):
+    print("Veritas")
+elif s.startswith("E"):
+    print("Exploration")
